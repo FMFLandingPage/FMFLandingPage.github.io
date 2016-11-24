15b2fbad384f7dd8a81393a1451ec9602854fd3a
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-11-24 16:04:30 -0500

    Added support for formspree

--- js/florida-custom.js
+++ js/florida-custom.js
@@ -162,35 +162,35 @@ else {
 /*	Contact Form
 /* ---------------------------------------------------------------------- */
 
-jQuery(document).ready(function(){
+// jQuery(document).ready(function(){
 								
-jQuery("button.btnSend").click(function(){
+// jQuery("button.btnSend").click(function(){
 
-var form = jQuery(this).closest('form');
+// var form = jQuery(this).closest('form');
 
-jQuery.ajax({type:'POST', url: 'php/contact.php', data:jQuery(form).serialize(), success: function(response) {
-
-
+// jQuery.ajax({type:'POST', url: 'php/contact.php', data:jQuery(form).serialize(), success: function(response) {
 
 
 
-	 if(parseInt(response)>0)
-	   {
-		 if(jQuery(form).find("#spanMessage").length)
-		 jQuery("#spanMessage").html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Well done!</strong> Your message has been sent.</div>');
-		 else
-		 alert('Well done! Your message has been sent');
-	   }
-	   else{
-		 if(jQuery(form).find("#spanMessage").length)
-		 jQuery("#spanMessage").html('<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Error! </strong> Somthing Wrong</div>');
-		 else
-		 alert('Somthing wrong!');
-	   }   
+
+
+// 	 if(parseInt(response)>0)
+// 	   {
+// 		 if(jQuery(form).find("#spanMessage").length)
+// 		 jQuery("#spanMessage").html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Well done!</strong> Your message has been sent.</div>');
+// 		 else
+// 		 alert('Well done! Your message has been sent');
+// 	   }
+// 	   else{
+// 		 if(jQuery(form).find("#spanMessage").length)
+// 		 jQuery("#spanMessage").html('<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Error! </strong> Somthing Wrong</div>');
+// 		 else
+// 		 alert('Somthing wrong!');
+// 	   }   
 		 
-}});
-});
-});
+// }});
+// });
+// });
 
 
 /* ---------------------------------------------------------------------- */
