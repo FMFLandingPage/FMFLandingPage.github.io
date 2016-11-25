1f460f43ed63115d4822fac777dcbcf1c1b93f09
FMFLandingPage <shameelfake@hotmail.com>
2016-11-24 22:51:01 -0500

    Update florida-custom.js

--- js/florida-custom.js
+++ js/florida-custom.js
@@ -143,11 +143,11 @@ else {
         		// console.log("Window top: " + $(window).scrollTop());
         		// console.log( $(".dividerDiv")[0].offsetTop);
         		// console.log(!$(".contactScroll").parent().hasClass("current"));
-        		if (!$(".contactScroll").parent().hasClass("current") && $(window).scrollTop() >= $(".contactPage")[0].offsetTop){
+        		if (!$(".contactScroll").parent().hasClass("current") && $(window).scrollTop() >= $(".contactPage").offsetTop){
         			$(".contactScroll").parent().addClass("current");
         			$(".homeScroll").parent().removeClass("current");
         		}
-        		else if ((!$(".homeScroll").parent().hasClass("current")) && $(window).scrollTop() < $(".contactPage")[0].offsetTop) {
+        		else if ((!$(".homeScroll").parent().hasClass("current")) && $(window).scrollTop() < $(".contactPage").offsetTop) {
         			$(".contactScroll").parent().removeClass("current");
         			$(".homeScroll").parent().addClass("current");
         		}
